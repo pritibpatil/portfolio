86daf62c37c08a6588979d183fbb59dd736e92fd
Priti Patil <81851169+pritibpatil@users.noreply.github.com>
2026-03-01 22:31:05 +0530

    Update HeaderSocials.jsx

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -5,8 +5,8 @@ import {FaGithub} from 'react-icons/fa'
 const HeaderSocials = () => {
   return (
     <div className='header__socials'>
-        <a href="https://www.linkedin.com/in/priti-patil-18913a233" target="_blank" target="_blank" rel="noreferrer noopener"><BsLinkedin/></a>
-        <a href="https://github.com/pritibpatil/" target="_blank" target="_blank" rel="noreferrer noopener"><FaGithub/></a>
+        <a href="https://www.linkedin.com/in/priti-patil-18913a233" target="_blank" rel="noreferrer noopener"><BsLinkedin/></a>
+        <a href="https://github.com/pritibpatil/" target="_blank" rel="noreferrer noopener"><FaGithub/></a>
     </div>
   )
 }
